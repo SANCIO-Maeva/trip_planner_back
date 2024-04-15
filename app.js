ca752b58b917781e00b9ca1fed9efe687ca678aa
Maeva SANCIO <maevasancio@lesperserverants.fr>
2024-04-15 15:03:25 +0200

    feat (app.js): create web server

---
+++ app.js
@@ -0,0 +1,27 @@
+import express from "express";
+
+import cors from "cors";
+
+import V1Router from "./routes/v1.js";
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:5500"],
+  })
+);
+
+app.use("/v1", V1Router);
+
+app.use((err, req, res, next) => {
+  res.status(err.status).json({ message: err.message });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
