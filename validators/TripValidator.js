091d061f67288c1c84a07bddda2e919f2f25c2e2
Maeva SANCIO <maevasancio@lesperserverants.fr>
2024-04-16 11:29:04 +0200

    fix(TripValidator):createdAt.optionnal

--- validators/TripValidator.js
+++ validators/TripValidator.js
@@ -3,7 +3,7 @@ import { z } from "zod";
 const TripValidator = z.object({
     prompt: z.string(),
     output: z.string().optional(),
-    createdAt: z.string().datetime(),
+    createdAt: z.string().datetime().optional(),
     updatedAt: z.string().datetime().optional(),
 });
 
