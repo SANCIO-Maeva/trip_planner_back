a2a185aade2c3471cc3d2cfb1eabd9051c06e144
Maeva SANCIO <maevasancio@lesperserverants.fr>
2024-04-17 09:28:16 +0200

    fix(cors): replace http://localhost:5173

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:5500"],
+    origin: ["http://localhost:5173"],
   })
 );
 
